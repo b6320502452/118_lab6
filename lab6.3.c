148f86414be161a00a01d169b31c1bd7121d4c35
b6320502452 <pavich.s@ku.th>
2021-03-15 19:09:24 +0700

    .

--- lab6.3.c
+++ lab6.3.c
@@ -1,17 +1,36 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<string.h>
 int main ()
 {
     int n,q;
     scanf("%d %d",&n,&q);
     int array[n];
     char com[2];
-    for(int j=0;j<N;j++)
+    int i,z,a,b;
+    for(int j=0;j<n;j++)
     {
         array[j]=0;
     }
-    for(int j=0;j<Q;j++)
+    for(int j=0;j<q;j++)
     {
-        scanf("%s",op);
-
+        scanf("%s",com);
+        if(toupper(com[0])=='U')
+        {
+            scanf("%d %d",&i,&z);
+            array[i]=z;
+        }
+        else if(toupper(com[0])=='P')
+        {
+            scanf("%d %d",&a,&b);
+            int max=array[a];
+            for(int k=a+1;k<=b;k++)
+            {
+                if(max<array[k])
+                {
+                    max=array[k];
+                }
+            }
+            printf("%d\n",max);
+        }
     }
 }
